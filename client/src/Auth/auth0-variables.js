80e8ba5036c18a9e6b9a4091b22b9c42510c8869
Jiweon Park <rkawk3142@gmail.com>
2020-03-27 15:13:09 -0700

    auth0 practice -- first commit

---
+++ client/src/Auth/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+  domain: 'flatfish.auth0.com',
+  clientId: 'vvAA66ArfYrchdt7xAcBC3FbkwEBInit',
+  callbackUrl: 'http://localhost:3000/callback',
+  apiUrl: 'www.fake.com',
+}
